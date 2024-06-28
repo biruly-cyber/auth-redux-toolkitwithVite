72917dc78609b3371906d235684e158a8962ee43
biruly-cyber <biruly2000@gmail.com>
2024-06-28 15:33:24 +0530

    implementation of redux

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,30 @@
-import express from "express"
+import express from "express";
+import { connectDatabase } from "../config/database.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
 
-const app = express()
+//routes
+import userRoute from "./routes/user.route.js";
 
-app.listen(3000, () =>{
-    console.log("server is listening on port 3000!")
-})
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+//connect db
+connectDatabase();
+
+//listen the port
+app.listen(3000, () => {
+  console.log("server is listening on port 3000!");
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API is working",
+  });
+});
+
+app.use("/api/v1/auth", userRoute);
