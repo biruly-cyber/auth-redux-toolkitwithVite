adcd4533fea71ebd10ab0eff60fe45862ff05be2
biruly-cyber <biruly2000@gmail.com>
2024-06-29 11:45:18 +0530

    protected the private route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 function App() {
   return (
     <BrowserRouter>
@@ -15,7 +16,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className="bg-blue-600 text-white p-4">
       <nav className="container mx-auto flex justify-between items-center">
         <div className="text-lg font-bold">MyApp</div>
-        <div className="space-x-4">
+        <div className="space-x-4 flex">
           <NavLink
             to="/"
             exact
@@ -20,27 +24,27 @@ const Header = () => {
           >
             About
           </NavLink>
-          <NavLink
-            to="/sign-in"
-            className="hover:underline"
-            activeClassName="underline"
-          >
-            Sign In
-          </NavLink>
-          <NavLink
-            to="/sign-up"
-            className="hover:underline"
-            activeClassName="underline"
-          >
-            Sign Up
-          </NavLink>
-          <NavLink
-            to="/profile"
-            className="hover:underline"
-            activeClassName="underline"
-          >
-            Profile
-          </NavLink>
+          {currentUser ? (
+            <NavLink
+              to="/profile"
+              className="hover:underline"
+              activeClassName="underline"
+            >
+              <img
+                src={currentUser.profilePicture}
+                alt="profile picture"
+                className="rounded-full h-7 w-7 object-cover"
+              />
+            </NavLink>
+          ) : (
+            <NavLink
+              to="/sign-in"
+              className="hover:underline"
+              activeClassName="underline"
+            >
+              Sign In
+            </NavLink>
+          )}
         </div>
       </nav>
     </header>
